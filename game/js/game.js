3cde92b65150ebefdf4d61a7cc934220c5ae2e75
Bruno Croci <crocidb@gmail.com>
2017-08-14 22:42:15 -0400

    basic initial game layout

---
+++ game/js/game.js
@@ -0,0 +1,20 @@
+class Game
+{
+    constructor()
+    {
+        
+    }
+
+    play()
+    {
+        console.log("WE DID IT, MAN");
+    }
+}
+
+(function() {
+    let game = new Game();
+    game.play();
+})();
+
+
+

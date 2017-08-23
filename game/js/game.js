87873bdb6013155ee7fa48f73aa26102e265317f
Bruno Croci <crocidb@gmail.com>
2017-08-22 23:47:23 -0400

    basic hatchEggs

--- game/js/game.js
+++ game/js/game.js
@@ -13,22 +13,23 @@ class Game {
 
         this.statusContainer = qSel("#status ul");
     
-        this.actionFindFood.onclick = this.findFood.bind(this);
-        this.actionHatchEggs.onclick = this.hatchEggs.bind(this);
-        this.actionBuildRoom.onclick = this.buildRoom.bind(this);
+        this.actionFindFood.onclick = this.doFindFood.bind(this);
+        this.actionHatchEggs.onclick = this.doHatchEggs.bind(this);
+        this.actionBuildRoom.onclick = this.doBuildRoom.bind(this);
     }
 
-    findFood() {
+    doFindFood() {
         showDialogWidget("Find Food", "<p>How far?</p><p>You'll need... </p>", [
             new Range(5, 85, 5, "Distance"),
             new Range(1, 5, 1, "Scouts")], this.sendScouts.bind(this), false);
     }
     
-    hatchEggs() {
-        showDialogOk("Hatch Eggs", "<p>How many eggs you want to hatch?</p>", () => {});
+    doHatchEggs() {
+        showDialogWidget("Hatch Eggs", "<p>How many eggs you want to hatch?</p>", [
+            new Range(1, 20, 1, "Eggs")], this.hatchEggs.bind(this));
     }
     
-    buildRoom() {
+    doBuildRoom() {
         showDialogOk("Build Room", "<p>You'll need X ants to build this room.</p>", () => {});
     }
 
@@ -37,6 +38,13 @@ class Game {
         let ants = values[1].val;
 
         let progress = new Progress("Scouts");
+        this.addProgress(progress);
+    }
+
+    hatchEggs(values) {
+        let eggs = values[0].val;
+
+        let progress = new Progress("Hatching Eggs");
         this.addProgress(progress);
     }
 
